97bdb536d5aad0ba7b77557a0b4246e6837d5b0b
Hoang Bao Ngo <bao88@live.no>
2018-05-09 14:23:28 +0200

    added Ideas.MD with reference to articles

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-05-08T12:20:01.532Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-05-09T09:02:23.542Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
